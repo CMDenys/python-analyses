d17e8daae91af7c59326abf0276ea8207dd80f79
Charlie Denys <charlie@Charlies-MBP.hsd1.il.comcast.net>
2021-01-05 09:57:02 -0600

    added working script for total revenue

--- PyBank/working/main.py
+++ PyBank/working/main.py
@@ -7,12 +7,14 @@ import csv
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
 
-month_count = []
+
 profit = []
 profit_change = []
 
 #create a starting point for row count
 num_rows = 0
+total_profits = 0
+
 
 #open and read data file
 with open(csvpath) as csvfile:
@@ -20,7 +22,14 @@ with open(csvpath) as csvfile:
     #loop through the rows to get a count. Found solution at https://www.kite.com/python/answers/how-to-count-the-number-of-lines-in-a-csv-file-in-python
     #remove header
     header = next(csvreader)
+
     for row in csvreader:
         num_rows += 1
+        total_profits += int(row[1])
+
+    print(f'Total Months: {num_rows}')    
+    print(f'Total Profits: {total_profits}')
+
+
     
-    print(f'Total Months: {num_rows}')
+
