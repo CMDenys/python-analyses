f0d03cd8e2b08a9a0e2c1a0702305976da23e93d
Charlie Denys <charlie@Charlies-MBP.hsd1.il.comcast.net>
2021-01-05 21:31:45 -0600

    reworked original by creating lists and appending

--- PyBank/working/main.py
+++ PyBank/working/main.py
@@ -3,17 +3,20 @@ import os
 import csv
 
 
+
 #create a path to the data file
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
 
 
-profit = []
-profit_change = []
+total_months = []
+profit_loss = []
+change_pl = []
 
 #create a starting point for row count
-num_rows = 0
 total_profits = 0
+profit_change = 0
+sum_changes = 0
 
 
 #open and read data file
@@ -23,13 +26,38 @@ with open(csvpath) as csvfile:
     #remove header
     header = next(csvreader)
 
+
     for row in csvreader:
-        num_rows += 1
-        total_profits += int(row[1])
-
-    print(f'Total Months: {num_rows}')    
-    print(f'Total Profits: {total_profits}')
+        total_months.append(row[0])
+        profit_loss.append(int(row[1]))
 
 
+
+    for i in range(1, len(profit_loss)):
+        #store differences in new list
+        change_pl.append(profit_loss[i] - profit_loss[i - 1])
+        avg_change = sum(change_pl) / len(change_pl)
+
+        greatest_profit = max(change_pl)
+        greatest_loss = min(change_pl)
+
+        #need to index into where the greatest profit happened in total months
+        max_date = change_pl.index(greatest_profit) + 1
+
+
+
+        
+
+
+print(f'Total Months: {len(total_months)}')    
+
+print(f'Total Profits: {sum(profit_loss)}')
+print(f'Average Change: {round(avg_change, 2)}')
+print(f'{total_months[max_date]} {greatest_profit}')
+print(f'{greatest_loss}')
+
+# print(f'{avg_net_change}')
+    
+   
     
 
